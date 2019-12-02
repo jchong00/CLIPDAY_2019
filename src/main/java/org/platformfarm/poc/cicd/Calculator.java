f8bc5989124c5c3493fa23014dca90aad421ed05
dpTablo <leeyw2355@gmail.com>
2019-12-02 15:17:13 +0900

    CLIPDAY19-5 #done CLIPDAY19-7 #done
    구현 완료.

--- src/main/java/org/platformfarm/poc/cicd/Calculator.java
+++ src/main/java/org/platformfarm/poc/cicd/Calculator.java
@@ -1,11 +1,11 @@
 package org.platformfarm.poc.cicd;
 
 public class Calculator {
-    public long add(int i1, int i2) {
-        return i1 + i2;
+    public long add(int op1, int op2) {
+        return op1 + op2;
     }
 
-    public long minus(int i, int i1) {
-        return i - i1;
+    public long minus(int op1, int op2) {
+        return op1 - op2;
     }
 }
